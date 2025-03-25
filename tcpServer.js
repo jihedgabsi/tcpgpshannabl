e38c3f3a717bcb4c935d0c702e705182abdae610
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-25 22:24:20 +0100

    Update tcpServer.js

--- tcpServer.js
+++ tcpServer.js
@@ -1,25 +1,10 @@
-const net = require('net');
+const net= require('net');
 const gt06 = require('./gt06n.js').gt06;
 
 const server = net.createServer((connection) => {
     connection.setEncoding('hex');
-
     connection.on('data', (data) => {
-        console.log('Donnée brute reçue:', data);
+        let result = gt06.parse(data);
 
-        // Conversion forcée en message de localisation (type 12)
-        try {
-            const forcedLocation = gt06.parseLocation(data);
-            console.log(`📍 Conversion forcée en type 12: 
-    Latitude: ${forcedLocation.latitude}, 
-    Longitude: ${forcedLocation.longitude}, 
-    Vitesse: ${forcedLocation.speed} km/h, 
-    Date & Heure: ${forcedLocation.datetime}`);
-        } catch (error) {
-            console.error("Erreur lors de la conversion en type 12 :", error);
-        }
     });
-
-}).listen(5000);
-
-console.log('🚀 Serveur GT06N actif sur le port 5000');
+}).listen(8090);
