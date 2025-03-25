2088452a9f98f9f9076b424432297392631f04b9
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-25 21:57:58 +0100

    Update tcpServer.js

--- tcpServer.js
+++ tcpServer.js
@@ -4,16 +4,20 @@ const gt06 = require('./gt06n.js').gt06; // Modification ici
 const server = net.createServer((connection) => {
     connection.setEncoding('hex');
     
-    connection.on('data', (data) => {
-        const result = gt06.parse(data); // Maintenant fonctionnel
-        console.log('Données décodées:', result);
-        
-        // Réponse au login
-        if (result.event === '01') {
-            const response = Buffer.from('787805010001D9DC0D0A', 'hex');
-            connection.write(response);
-        }
-    });
+   connection.on('data', (data) => {
+    const result = gt06.parse(data);
+    console.log('Données décodées:', result);
+
+    if (result.event === '12') { // Position
+        console.log(`Position reçue: Latitude ${result.parsed.latitude}, Longitude ${result.parsed.longitude}`);
+    }
+
+    if (result.event === '01') { // Réponse au login
+        const response = Buffer.from('787805010001D9DC0D0A', 'hex');
+        connection.write(response);
+    }
+});
+
     
 }).listen(5000);
 
