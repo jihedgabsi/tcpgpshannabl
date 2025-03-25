f5e961677d958bb017e33bcab50725c7122b91d5
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-25 22:24:37 +0100

    Update gt06n.js

--- gt06n.js
+++ gt06n.js
@@ -2,7 +2,7 @@ module.exports.gt06 = (function(){
     const eventTypes = {
         '01' : 'Login Message',
         '12' : 'Location Data',
-        '13' : 'Status information',
+        '13' : 'Status information ',
         '15' : 'String information',
         '16' : 'Alarm data',
         '1A' : 'GPS, query address information by phone number',
@@ -10,16 +10,21 @@ module.exports.gt06 = (function(){
     };
 
     const parseLogin = (data) => {
-        return {
+        let parsed;
+
+        parsed = {
             'eventType': eventTypes['01'],
-            'imei': parseInt(data.substr(8,16), 16),
+            'imei': parseInt(data.substr(8,16),16),
             'serialNumber': data.substr(22,4),
             'errorCheck': data.substr(26,4),
+
         };
+        return parsed;
     };
 
     const parseDatetime = (data) => {
-        const parts = {
+        let parse, y, m, d, h, i, s, datetime;
+        parse = {
             'year': data.substr(0,2),
             'month': data.substr(2,2),
             'day': data.substr(4,2),
@@ -28,35 +33,37 @@ module.exports.gt06 = (function(){
             'second': data.substr(10,2),
         };
 
-        const y = parseInt(parts.year, 16);
-        const m = parseInt(parts.month, 16);
-        const d = parseInt(parts.day, 16);
-        const h = parseInt(parts.hour, 16);
-        const i = parseInt(parts.minute, 16);
-        const s = parseInt(parts.second, 16);
-        return `${y}-${m}-${d} ${h}:${i}:${s}`;
+        y = parseInt(parse.year, 16);
+        m = parseInt(parse.month, 16);
+        d = parseInt(parse.day, 16);
+        h = parseInt(parse.hour, 16);
+        i = parseInt(parse.minute, 16);
+        s = parseInt(parse.second, 16);
+        datetime = [y,m,d].join('-') + ' ' +  [h,i,s].join(':');
+        return datetime;
     };
 
-    const hex2bin = (hex) => {
-        let bin = (parseInt(hex, 16)).toString(2);
-        const hexLen = hex.length / 2 * 8;
-        while(bin.length < hexLen){
-            bin = '0' + bin;
+    const hex2bin =  (hex) => {
+        let bin, hexLen;
+
+        bin = (parseInt(hex, 16)).toString(2);
+        hexLen = hex.length/2*8;
+
+        for( let i =0; i < 8; i++){
+            if(bin.length < hexLen){
+                bin = '0' + bin;
+            }
         }
         return bin;
     };
 
-    const parseCoordinate = (hex, direction) => {
-        let value = parseInt(hex, 16) / 1800000; // Conversion adaptée selon le protocole
-        if (direction === 'S' || direction === 'W') {
-            value *= -1;
-        }
-        return value;
-    };
-
     const parseLocation = (data) => {
-        // Ici, nous supposons que la trame est au moins conforme à celle du type 12 (Location Data)
-        const datasheet = {
+        let datasheet, parsed, courseBin;
+
+        datasheet = {
+            'start_bit': data.substr(0,4),
+            'protocol_length': data.substr(4,2),
+            'protocol_number': data.substr(6,2),
             'datetime': data.substr(8,12),
             'quantity': data.substr(20,2),
             'lat': data.substr(22,8),
@@ -72,32 +79,33 @@ module.exports.gt06 = (function(){
             'stop_bit': data.substr(68,4),
         };
 
-        const courseBin = hex2bin(datasheet.course);
-        // Pour la direction, on suppose :
-        // - northLatitude : bit à l'index 5 (1 = Nord, 0 = Sud)
-        // - eastLongitude : bit à l'index 4 (1 = Est, 0 = Ouest)
-        const northLatitude = courseBin.substr(5, 1) === '1';
-        const eastLongitude  = courseBin.substr(4, 1) === '1';
+        courseBin = hex2bin(datasheet.course);
 
-        return {
+        parsed = {
             'datetime': parseDatetime(datasheet.datetime),
-            'satellites': parseInt(datasheet.quantity.substr(0,1), 16),
-            'latitude': parseCoordinate(datasheet.lat, northLatitude ? 'N' : 'S'),
-            'longitude': parseCoordinate(datasheet.lon, eastLongitude ? 'E' : 'W'),
-            'speed': parseInt(datasheet.speed, 16),
-            'direction': parseInt(courseBin.substr(6,10), 2),
-            'mcc': datasheet.mcc,
-            'mnc': datasheet.mnc,
-            'lac': datasheet.lac,
+            'satelites': parseInt(datasheet.quantity.substr(0,1),16),
+            'satelitesActive': parseInt(datasheet.quantity.substr(1,1),16),
+            'lat': datasheet.lat,
+            'lon': datasheet.lon,
+            'speed': parseInt(datasheet.speed,16),
+            'real_time_gps': courseBin.substr(2,1),
+            'gps_positioned': courseBin.substr(3,1),
+            'east_longitude': courseBin.substr(4,1),
+            'north_latitude': courseBin.substr(5,1),
+            'course': parseInt(courseBin.substr(6,10),2),
+            'mmc': datasheet.mnc,
             'cell_id': datasheet.cell_id,
             'serial_number': datasheet.serial_number,
             'error_check': datasheet.error_check,
             'stop_bit': datasheet.stop_bit,
         };
+        return parsed;
     };
 
     const parseAlarm = (data) => {
-        const datasheet = {
+        let datasheet, courseBin, parsed;
+
+        datasheet = {
             'start_bit': data.substr(0,4),
             'protocol_length': data.substr(4,2),
             'protocol_number': data.substr(6,2),
@@ -119,20 +127,26 @@ module.exports.gt06 = (function(){
             'stop_bit': data.substr(76,4),
         };
 
-        const courseBin = hex2bin(datasheet.course);
-        return {
+        courseBin = hex2bin(datasheet.course);
+        parsed = {
             'datetime': parseDatetime(datasheet.datetime),
-            'satellites': parseInt(datasheet.quantity.substr(0,1), 16),
-            'latitude': parseCoordinate(datasheet.lat, courseBin.substr(5,1) === '1' ? 'N' : 'S'),
-            'longitude': parseCoordinate(datasheet.lon, courseBin.substr(4,1) === '1' ? 'E' : 'W'),
-            'speed': parseInt(datasheet.speed, 16),
-            'direction': parseInt(courseBin.substr(6,10), 2),
-            'mcc': datasheet.mnc,
+            'satelites': parseInt(datasheet.quantity.substr(0,1),16),
+            'satelitesActive': parseInt(datasheet.quantity.substr(1,1),16),
+            'lat': datasheet.lat,
+            'lon': datasheet.lon,
+            'speed': parseInt(datasheet.speed,16),
+            'real_time_gps': courseBin.substr(2,1),
+            'gps_positioned': courseBin.substr(3,1),
+            'east_longitude': courseBin.substr(4,1),
+            'north_latitude': courseBin.substr(5,1),
+            'course': parseInt(courseBin.substr(6,10),2),
+            'mmc': datasheet.mnc,
             'cell_id': datasheet.cell_id,
             'serial_number': datasheet.serial_number,
             'error_check': datasheet.error_check,
             'stop_bit': datasheet.stop_bit,
         };
+        return parsed;
     };
 
     const parseStatusPackage = (courseBin) => {
@@ -142,54 +156,54 @@ module.exports.gt06 = (function(){
             'east_longitude': courseBin.substr(4,1),
             'north_latitude': courseBin.substr(5,1),
             'course': parseInt(courseBin.substr(6,10),2)
-        };
+        }
     };
 
     const parseStatus = (data) => {
-        const statusBin = hex2bin(data.substr(8,6));
-        return parseStatusPackage(statusBin);
-    };
+        let statusBin, parsed;
+        statusBin = hex2bin(data.substr(8,6));
+        parsed = parseStatusPackage(statusBin);
+        return parsed;
 
-    const selectEvent = (data) => {
-        return data.substr(6,2);
     };
-
     const parse = (data) => {
         let response;
-        const event = selectEvent(data);
-        switch (event) {
+        switch (selectEvent(data)){
             case '01':
                 response = parseLogin(data);
                 break;
             case '12':
                 response = parseLocation(data);
                 break;
+
             case '13':
                 response = parseStatus(data);
                 break;
             case '15':
-                // À implémenter si nécessaire
+                //parseLocation(data);
                 break;
             case '16':
                 response = parseAlarm(data);
                 break;
             case '1A':
-                // À implémenter si nécessaire
+                //parseLocation(data);
                 break;
             case '80':
-                // À implémenter si nécessaire
-                break;
-            default:
-                response = undefined;
+                //parseLocation(data);
                 break;
         }
         return {
             'data_packet': data,
-            'event': event,
+            'event': selectEvent(data),
             'parsed': response
         };
     };
 
-    // On exporte également la fonction parseLocation pour conversion forcée
-    return { parse: parse, parseLocation: parseLocation };
+    const selectEvent = (data) => {
+        return data.substr(6,2);
+    };
+
+
+
+    return { parse:parse };
 })();
