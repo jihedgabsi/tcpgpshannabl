900b47cd49632d7001b27e9e9e5c81f3fda0c4c3
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-25 22:38:24 +0100

    Update tcpServer.js

--- tcpServer.js
+++ tcpServer.js
@@ -1,57 +1,10 @@
-const net = require('net');
-const GpsData = require('./models/GpsData');
-const dotenv = require('dotenv');
-require('dotenv').config();
-const connectDB = require('./config/database');
+const net	= require('net');
+const gt06 = require('./gt06n.js').gt06n;
 
-dotenv.config();
-connectDB();
+const server = net.createServer((connection) => {
+    connection.setEncoding('hex');
+    connection.on('data', (data) => {
+        let result = gt06.parse(data);
 
-const server = net.createServer(socket => {
-    console.log('Nouvelle connexion TCP');
-
-    socket.on('data', async data => {
-        const message = data.toString().trim();
-        console.log(`Données reçues : ${message}`);
-
-        // Exemple de format attendu : "123456,48.8566,2.3522,30"
-        const parts = message.split(',');
-
-        if (parts.length === 4) {
-            const deviceId = parts[0];
-            const latitude = parseFloat(parts[1]);
-            const longitude = parseFloat(parts[2]);
-            const speed = parseFloat(parts[3]);
-
-            try {
-                // Vérifier si le deviceId existe déjà
-                const existingGpsEntry = await GpsData.findOne({ deviceId });
-
-                if (existingGpsEntry) {
-                    // Mise à jour des données existantes
-                    await GpsData.updateOne(
-                        { deviceId },
-                        { latitude, longitude, speed, updatedAt: new Date() }
-                    );
-                    console.log("Données GPS mises à jour !");
-                } else {
-                    // Création d'un nouvel enregistrement si deviceId n'existe pas
-                    const gpsEntry = new GpsData({ deviceId, latitude, longitude, speed });
-                    await gpsEntry.save();
-                    console.log("Nouvelles données GPS enregistrées !");
-                }
-            } catch (error) {
-                console.error("Erreur lors de l'enregistrement des données GPS :", error);
-            }
-        } else {
-            console.log("Format incorrect");
-        }
     });
-
-    socket.on('error', err => console.error("Erreur socket :", err));
-    socket.on('end', () => console.log("Connexion terminée"));
-});
-
-// Démarrer le serveur TCP
-const PORT = process.env.TCP_PORT || 5000;
-server.listen(PORT, '0.0.0.0', () => console.log(`Serveur TCP en écoute sur le port ${PORT}`));
+}).listen(5000);
